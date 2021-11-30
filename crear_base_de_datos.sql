a4669469780b2f000dfba3e4b713a82e64f33581
LuiggiAndresZambranoFranco <74325889+LuiggiAndresZambranoFranco@users.noreply.github.com>
2021-11-30 02:54:41 -0500

    Create crear_base_de_datos.sql

---
+++ crear_base_de_datos.sql
@@ -0,0 +1,12 @@
+-- Database: centro_terapias
+
+-- DROP DATABASE centro_terapias;
+
+CREATE DATABASE centro_terapias
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
